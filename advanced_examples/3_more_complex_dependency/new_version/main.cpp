215c86052c9972910bc97c45d91e50a1caea33af
ys-2020 <yangshan19@mails.tsinghua.edu.cn>
2022-05-29 09:40:47 +0000

    first commit

---
+++ advanced_examples/3_more_complex_dependency/new_version/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+#include"add.h" 
+
+
+int main()
+{
+    int c = add(1,3);
+    cout << c <<endl;
+    return 0;
+}
---
+++ advanced_examples/3_more_complex_dependency/old_version/add.h
@@ -0,0 +1,4 @@
+#ifndef __add_H
+#define __add_H
+int add(int a,int b);
+#endif
